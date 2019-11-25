47f21fcfbc1a51f3e389325793364ab7c451bcc6
Brandon Smyth <bjsmyth@umich.edu>
2019-11-25 16:01:45 -0500

    Update train.py

--- train.py
+++ train.py
@@ -40,12 +40,12 @@ def get_train_data():
         # valid_lr_img_list = sorted(tl.files.load_file_list(path=config.VALID.lr_img_path, regx='.*.png', printable=False))
 
     ## If your machine have enough memory, please pre-load the entire train set.
-    train_hr_imgs1 = tl.vis.read_images(train_hr_img_list, path=config.TRAIN.hr_img_path, n_threads=32)
-    train_hr_imgs = []
-    for im in train_hr_imgs1: #Convert greyscale to RGB
-      train_hr_imgs.append(np.stack((im,)*3, axis=-1))
-        # for im in train_hr_imgs:
-        #     print(im.shape)
+    train_hr_imgs = tl.vis.read_images(train_hr_img_list, path=config.TRAIN.hr_img_path, n_threads=32)
+    #train_hr_imgs = []
+    #for im in train_hr_imgs1: #Convert greyscale to RGB
+    #  train_hr_imgs.append(np.stack((im,)*3, axis=-1))
+    #for im in train_hr_imgs:
+    #    print(im.shape)
         # valid_lr_imgs = tl.vis.read_images(valid_lr_img_list, path=config.VALID.lr_img_path, n_threads=32)
         # for im in valid_lr_imgs:
         #     print(im.shape)
@@ -58,7 +58,8 @@ def get_train_data():
         for img in train_hr_imgs:
             yield img
     def _map_fn_train(img):
-        hr_patch = tf.image.random_crop(img, [384, 384, 3])
+        hr_patch = tf.image.random_crop(img, [384, 384])
+        hr_patch = tf.expand_dims(hr_patch, -1)
         hr_patch = hr_patch / (255. / 2.)
         hr_patch = hr_patch - 1.
         hr_patch = tf.image.random_flip_left_right(hr_patch)
@@ -74,8 +75,8 @@ def get_train_data():
     return train_ds
 
 def train():
-    G = get_G((batch_size, 96, 96, 3))
-    D = get_D((batch_size, 384, 384, 3))
+    G = get_G((batch_size, 96, 96, 1))
+    D = get_D((batch_size, 384, 384, 1))
     VGG = tl.models.vgg19(pretrained=True, end_with='pool4', mode='static')
 
     lr_v = tf.Variable(lr_init)
@@ -163,28 +164,40 @@ def evaluate():
     # for im in train_hr_imgs:
     #     print(im.shape)
     valid_lr_imgs = tl.vis.read_images(valid_lr_img_list, path=config.VALID.lr_img_path, n_threads=32)
+    #valid_lr_imgs = [tf.expand_dims(valid_lr_imgs_i, -1) for valid_lr_imgs_i in valid_lr_imgs]
     # for im in valid_lr_imgs:
     #     print(im.shape)
     valid_hr_imgs = tl.vis.read_images(valid_hr_img_list, path=config.VALID.hr_img_path, n_threads=32)
+    #valid_lr_imgs = [tf.expand_dims(valid_hr_imgs_i, -1) for valid_hr_imgs_i in valid_hr_imgs]
     # for im in valid_hr_imgs:
     #     print(im.shape)
 
     ###========================== DEFINE MODEL ============================###
-    imid = 64  # 0: 企鹅  81: 蝴蝶 53: 鸟  64: 古堡
+    imid = 20
     valid_lr_img = valid_lr_imgs[imid]
     valid_hr_img = valid_hr_imgs[imid]
     # valid_lr_img = get_imgs_fn('test.png', 'data2017/')  # if you want to test your own image
     valid_lr_img = (valid_lr_img / 127.5) - 1  # rescale to ［－1, 1]
-    # print(valid_lr_img.min(), valid_lr_img.max())
+    
+    valid_lr_img = np.asarray(valid_lr_img, dtype=np.float32)
+    valid_lr_img = valid_lr_img[np.newaxis,:,:]
+    size = [valid_lr_img.shape[1], valid_lr_img.shape[2]]
 
-    G = get_G([1, None, None, 3])
+    out_bicu = scipy.misc.imresize(valid_lr_img[0], [size[0] * 4, size[1] * 4], interp='bicubic', mode=None)
+    tl.vis.save_image(out_bicu, os.path.join(save_dir, 'valid_bicubic.png'))
+
+    valid_lr_img = valid_lr_imgs[imid]
+    valid_hr_img = valid_hr_imgs[imid]
+    valid_lr_img = (valid_lr_img / 127.5) - 1  # rescale to ［－1, 1]
+    # print(valid_lr_img.min(), valid_lr_img.max())
+    valid_lr_img = tf.expand_dims(valid_lr_img, -1)
+
+    G = get_G([1, None, None, 1])
     G.load_weights(os.path.join(checkpoint_dir, 'g.h5'))
     G.eval()
 
     valid_lr_img = np.asarray(valid_lr_img, dtype=np.float32)
     valid_lr_img = valid_lr_img[np.newaxis,:,:,:]
-    size = [valid_lr_img.shape[1], valid_lr_img.shape[2]]
-
     out = G(valid_lr_img).numpy()
 
     print("LR size: %s /  generated HR size: %s" % (size, out.shape))  # LR size: (339, 510, 3) /  gen HR size: (1, 1356, 2040, 3)
@@ -193,8 +206,6 @@ def evaluate():
     tl.vis.save_image(valid_lr_img[0], os.path.join(save_dir, 'valid_lr.png'))
     tl.vis.save_image(valid_hr_img, os.path.join(save_dir, 'valid_hr.png'))
 
-    out_bicu = scipy.misc.imresize(valid_lr_img[0], [size[0] * 4, size[1] * 4], interp='bicubic', mode=None)
-    tl.vis.save_image(out_bicu, os.path.join(save_dir, 'valid_bicubic.png'))
 
 
 if __name__ == '__main__':
